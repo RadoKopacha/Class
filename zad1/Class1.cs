c72fd8cdb2ac623c9b4e41cdcfd707aa4b50c201
RadoKopacha <petyrnikolov0804@gmail.com>
2024-04-09 22:22:52 +0300

    first commit

---
+++ zad1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    internal class Class1
+    {
+        public class Person
+        {
+            public string name;
+            public int age;
+            public void Eat()
+            {
+                Console.WriteLine(name + " nqm, nqm vkusno");
+            }
+        }
+    }
+}
